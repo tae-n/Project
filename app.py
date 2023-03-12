7a4dc24b0a968e0ba2830c9f449dd3a95cd293d1
tae-n <127629283+tae-n@users.noreply.github.com>
2023-03-12 14:21:32 -0700

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,11 @@ st.table(pd.DataFrame({
     'Weather': ['Clear', 'Raining', 'Overcast', 'Other'],
     'Count': [2494, 1630, 809, 180]}))
  
-label_w26 = 'CLear', 'Raining', 'Overcast', 'Other'
+label_w26 = 'Clear', 'Raining', 'Overcast', 'Other'
 size_w26 = [2494, 1630, 809, 180]
-
 fig_w26, ax_w26 = plt.subplots()
 ax_w26.pie(size_w26, labels=label_w26, autopct='%1.1f%%', startangle=90)
-ax_w26.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
+ax_w26.axis('equal')  
 st.pyplot(fig_w26)
    
 st.write("Road Condition")
@@ -26,8 +24,22 @@ st.table(pd.DataFrame({
     'Road': ['Dry', 'Wet', 'Other'],
     'Count': [2728, 2130, 97]}))
 
+label_r26 = 'Dry', 'Wet', 'Other'
+size_r26 = [2728, 2130, 97]
+fig_r26, ax_r26 = plt.subplots()
+ax_r26.pie(size_r26, labels=label_r26, autopct='%1.1f%%', startangle=90)
+ax_r26.axis('equal')  
+st.pyplot(fig_r26)
+
 st.write("Light Conditions")
 
 st.table(pd.DataFrame({
     'Light': ['Dark', 'Daylight', 'Dawn', 'Dusk', 'Other'],
     'Count': [2604, 2175, 129, 128, 88]}))
+
+label_l26 = 'Dark', 'Daylight', 'Dawn', 'Dusk', 'Other'
+size_l26 = [2604, 2175, 129, 128, 88]
+fig_l26, ax_l26 = plt.subplots()
+ax_l26.pie(size_l26, labels=label_l26, autopct='%1.1f%%', startangle=90)
+ax_l26.axis('equal')  
+st.pyplot(fig_l26)
